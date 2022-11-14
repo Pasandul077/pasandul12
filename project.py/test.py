02f10cdd2126668105538d78e59c98268f0f9227
Pasandul077 <118114100+Pasandul077@users.noreply.github.com>
2022-11-14 22:16:38 +0530

    Add files via upload

---
+++ project.py/test.py
@@ -0,0 +1,74 @@
+#Please enter your name here
+user_name=input('Please enter your name here')
+
+#Whats your vehicle type? Car , Bike or SUV
+user_vehicle_type=input('Whats your vehicle type? Car , Bike or SUVe')
+
+#Mention the fuel tank capacity
+vehicle_fuel_tank_capacity=input('Mention the fuel tank capacity')
+
+#Whats your fuel type? 92 , 95 or Super Diesel
+fuel_type=input('Whats your fuel type? 92 , 95 or Super Diesel')
+
+#Enter fuel price
+fuel_price=input('Enter fuel price')
+
+#whats your previous meter reading?
+previous_meter_reading=input('whats your previous meter reading?')
+
+#whats your current meter reading?
+current_meter_reading=input('whats your current meter reading?')
+
+#How many fuel you pumped now
+fuel_pumped_now=input('How many fuel you pumped now')
+
+
+
+subtraction='previous_meter_reading' - 'current_meter_reading'
+mutiplication= 'fuel_price' * 'fuel_pumped_now'
+division='subtraction' / 'fuel_pumped_now'
+
+def check_user_vehicle_typ(user_vehicle_type):
+ if  user_vehicle_type == 'car':
+                            print("car")
+                            print('hi' + user_name ) 
+                            print(previous_meter_reading)
+                            print(current_meter_reading)
+                            print(fuel_type)
+                            print(fuel_price)
+                            print(mutiplication)
+                            print(division)
+ else : print('sorry try agin!')
+
+ check_user_vehicle_type(user_vehicle_type):
+ 
+
+ def check_user_vehicle_typ(user_vehicle_type):
+                if  user_vehicle_type == 'bike':
+                            print("bike")
+                            print('hi' + user_name )
+                            print(previous_meter_reading)
+                            print(current_meter_reading)
+                            print(fuel_type)
+                            print(fuel_price)
+                            print(mutiplication)
+                            print(division)
+                 else : print('sorry try agin!')
+
+ check_user_vehicle_type=(user_vehicle_type):
+
+ def check_user_vehicle_typ(user_vehicle_type):
+                if  user_vehicle_type == 'SUV':
+                            print("SUV")
+                            print('hi' + user_name )
+                            print(previous_meter_reading)
+                            print(current_meter_reading)
+                            print(fuel_type)
+                            print(fuel_price)
+                            print(mutiplication)
+                            print(division)
+                else : print('sorry try agin!')
+
+check_user_vehicle_type=(user_vehicle_type):
+
+
